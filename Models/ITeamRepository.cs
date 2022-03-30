27e71b1f62b2690c7c1fcf8b09b27fa5e748a31b
ShengLei123 <starkray666666@gmail.com>
2022-03-30 16:37:52 -0600

    Add project files.

---
+++ Models/ITeamRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mssion13.Models
+{
+    public interface ITeamRepository
+    {
+        IQueryable<Team> Teams { get; }
+    }
+}
